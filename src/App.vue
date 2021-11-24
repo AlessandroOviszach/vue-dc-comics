a25f12e0e8c86c897888e0fab61e7ca187c975f7
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-24 17:01:20 +0100

    define header sections

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-
-
     <Header />
 
     <Main />
@@ -28,12 +26,16 @@ export default {
 
 // App general styles  
 <style lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px-;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,28 @@
 <template>
+    <header class="container">
+        <!-- Logo section -->
+        <div class="logo">
+            <img src="../assets/dc-logo.png" alt="DC logo">
+        </div>
 
-    <h1>Header section Here</h1>
+        <!-- Menu section -->
+        <div class="menu">
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectionables</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </div>
+
+
+    </header>
 
 </template>
 
@@ -10,6 +32,37 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+    .container {
+        background-color: #2c3e50;;
+        display: flex;
+        align-items: center;
+        height: 100px;
 
+        .logo {
+            display: flex;
+            justify-content: center;
+            width: 45%;
+            padding: 1rem;
+            
+            img {
+            height: 100%;
+            }
+        }
+
+
+        .menu {
+            height: 100%;
+            background-color: tomato;
+            ul {
+                display: flex;
+                align-items: center;
+                list-style: none;
+                li {
+                    height: 100%;
+                    text-transform: uppercase;
+                }
+            }
+        }
+    }
 </style>
