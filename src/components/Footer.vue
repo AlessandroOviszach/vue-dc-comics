9176925b25b8384ab15d982360eaf21935359498
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-25 17:34:02 +0100

    footer check

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,67 +1,99 @@
 <template>
 
     <footer>
-        <div class="container">
-            <!-- link utili -->
-            <div class="links">
-                
-                <!-- DC COMICS -->
-                <div>
-                    <h1>Dc comics</h1>
-                    <ul>
-                        <li>characters</li>
-                        <li>comics</li>
-                        <li>movies</li>
-                        <li>tv</li>
-                        <li>games</li>
-                        <li>videos</li>
-                        <li>news</li>
-                    </ul>
+        <div class="links-bg">
+            <div class="container">
+                <!-- link utili -->
+                <div class="links">
+
+                    <!-- DC COMICS -->
+                    <div>
+                        <h1>Dc comics</h1>
+                        <ul>
+                            <li>characters</li>
+                            <li>comics</li>
+                            <li>movies</li>
+                            <li>tv</li>
+                            <li>games</li>
+                            <li>videos</li>
+                            <li>news</li>
+                        </ul>
+                    </div>
+
+                    <!-- SHOP -->
+                    <div>
+                        <h1>Shop</h1>
+                        <ul>
+                            <li>Shop Dc</li>
+                            <li>Shop Dc Collectionables</li>
+                        </ul>
+                    </div>
+
+                    <!-- DC -->
+                    <div>
+                        <h1>Dc</h1>
+                        <ul>
+                            <li>Terms of use</li>
+                            <li>privacy policy (new)</li>
+                            <li>ad choice</li>
+                            <li>advertising</li>
+                            <li>jobs</li>
+                            <li>subscriptions</li>
+                            <li>takent workshops</li>
+                            <li>CPSC certificates</li>
+                            <li>ratings</li>
+                            <li>shop help</li>
+                            <li>contact us</li>
+                        </ul>
+                    </div>
+
+                    <!-- SITES -->
+                    <div>
+                        <h1>Sites</h1>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD magazine</li>
+                            <li>DC kids</li>
+                            <li>DC universe</li>
+                            <li>DC power visa</li>
+                        </ul>
+                    </div>
                 </div>
 
-                <!-- SHOP -->
-                <div>
-                    <h1>Shop</h1>
-                    <ul>
-                        <li>Shop Dc</li>
-                        <li>Shop Dc Collectionables</li>
-                    </ul>
-                </div>
-
-                <!-- DC -->
-                <div>
-                    <h1>Dc</h1>
-                    <ul>
-                        <li>Terms of use</li>
-                        <li>privacy policy (new)</li>
-                        <li>ad choice</li>
-                        <li>advertising</li>
-                        <li>jobs</li>
-                        <li>subscriptions</li>
-                        <li>takent workshops</li>
-                        <li>CPSC certificates</li>
-                        <li>ratings</li>
-                        <li>shop help</li>
-                        <li>contact us</li>
-                    </ul>
-                </div>
-
-                <!-- SITES -->
-                <div>
-                    <h1>Sites</h1>
-                    <ul>
-                        <li>DC</li>
-                        <li>MAD magazine</li>
-                        <li>DC kids</li>
-                        <li>DC universe</li>
-                        <li>DC power visa</li>
-                    </ul>
-                </div>
             </div>
 
-            <!-- contatti -->
-            <div class="contacts">
-                <a href="">SIGN-UP NOW!</a>
+        </div>
+        
+        <div class="contacts-bg">
+            <div class="container footer-contact">
+                <!-- contatti -->
+                <div class="contacts">
+                    <a href="">SIGN-UP NOW!</a>
+                </div>
+
+                <div class="social">
+                    <h2>Follow us</h2>
+
+                    <div class="img-container">
+                        <img src="@/assets/footer-facebook.png" alt="">
+                    </div>
+
+                    <div class="img-container">
+                        <img src="@/assets/footer-twitter.png" alt="">
+                    </div>
+
+                    <div class="img-container">
+                        <img src="@/assets/footer-youtube.png" alt="">
+                    </div>
+
+                    <div class="img-container">
+                        <img src="@/assets/footer-pinterest.png" alt="">
+                    </div>
+
+                    <div class="img-container">
+                        <img src="@/assets/footer-periscope.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
@@ -82,13 +114,16 @@ export default {
         background-color: $dark-gray;
         color: white;
         max-height: auto;
-        background-image: url('../assets/footer-bg.jpg');
-        background-repeat: no-repeat;
-        background-position: top;
-        background-size: cover;
+
+        .links-bg {
+            background-image: url('../assets/footer-bg.jpg');
+            background-repeat: no-repeat;
+            background-position: top;
+            background-size: cover;
+        }
 
         .links {
-            padding: 1rem;
+            padding: 1rem 0;
             display: flex;
             flex-direction: column;
             flex-wrap: wrap;
@@ -113,15 +148,35 @@ export default {
             }
         }
 
-        .contacts {
-            background-color: $dark-gray;
-            a {
-                text-decoration: none;
-                color: currentColor;
-                text-transform: uppercase;
-                padding: 0.5rem;
-                border: 2px solid $primary-color;
+        .footer-contact {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            .contacts {
+                background-color: $dark-gray;
+                padding: 2.5rem 0;
+                a {
+                    text-decoration: none;
+                    color: currentColor;
+                    text-transform: uppercase;
+                    padding: 0.5rem;
+                    border: 2px solid $primary-color;
+                }
+            }
+
+            .social {
+                display: flex;
+                align-items: center;
+                h2 {
+                    color: $primary-color;
+                    margin-right: 1rem;
+                    text-transform: uppercase;
+                }
+                .img-container {
+                    margin: 0 0.5rem;
+                }
             }
         }
+
     }
 </style>
